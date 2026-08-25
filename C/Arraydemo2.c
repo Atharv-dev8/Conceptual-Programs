92bf5033403379d750b7bd7c0d8a2ee380b630f1
Atharv Mahadik <atharvofficial917@gmail.com>
2026-08-26 01:44:43 +0530

    Performed Array operations

---
+++ C/Arraydemo2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+int Arr[4] = {10,20,30,40};
+
+printf("%d\n",Arr);            //100
+printf("%d\n",&Arr);           //100
+
+                                
+printf("%d\n",Arr+1);      //100
+printf("%d\n",&(Arr)+1);
+
+
+
+
+
+
+    return 0;
+}
